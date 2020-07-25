2b795a2bfe2eb648e117ddaff860a767e0607ab0
Timothy J Fontaine <tjfontaine@gmail.com>
2020-07-25 11:01:01 -0700

    fix recaptcha

--- assets/js/recaptcha.js
+++ assets/js/recaptcha.js
@@ -1,8 +1,6 @@
-<script>
    grecaptcha.ready(function() {
        grecaptcha.execute('6LexG7YZAAAAAFh-J3uSH5R1XCwrjn-mmDD6rZQr', {action: 'homepage'})
        .then(function(token) {
          document.getElementById('captchaResponse').value = token;
        });
      });
-</script>
